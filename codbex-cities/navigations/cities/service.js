c467fa638c99718f5b46fae352ffd09b17375cd4
TIVMOF <tomyivanov06@gmail.com>
2024-12-04 16:05:24 +0000

    fix: Navigations

--- codbex-cities/navigations/cities/service.js
+++ codbex-cities/navigations/cities/service.js
@@ -1,7 +1,7 @@
 const navigationData = {
     id: 'cities-navigation',
     label: "Cities",
-    group: "reference-data",
+    group: "metadata",
     order: 500,
     link: "/services/web/codbex-cities/gen/codbex-cities/ui/Cities/index.html?embedded"
 };

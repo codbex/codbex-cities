f9bfad21d07a069161a7d485105a4150da2d56ac
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2024-12-05 15:04:36 +0200

    New dashboard structure (#5)

--- codbex-cities/navigations/cities/service.js
+++ codbex-cities/navigations/cities/service.js
@@ -1,10 +1,8 @@
 const navigationData = {
     id: 'cities-navigation',
     label: "Cities",
-    view: "cities",
-    group: "configurations",
-    orderNumber: 1000,
-    lazyLoad: true,
+    group: "metadata",
+    order: 500,
     link: "/services/web/codbex-cities/gen/codbex-cities/ui/Cities/index.html?embedded"
 };
 
